5089b17e8aa29ea084e96b51d11a194c6e044ef8
razhayat <razh.graffiti@gmail.com>
2024-08-09 10:38:34 +0300

    Added GuardedTuple util type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,7 @@
 export type TypeGuard<in out T> = (value: unknown) => value is T;
+
 export type Guarded<T extends TypeGuard<any>> = T extends TypeGuard<infer R> ? R : never;
+
+export type GuardedTuple<T extends readonly TypeGuard<any>[]> = {
+	[I in keyof T]: Guarded<T[I]>;
+};
