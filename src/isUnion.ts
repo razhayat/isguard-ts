cc6beb5b10647bcd09e408a5495229658888e76c
razhayat <razh.graffiti@gmail.com>
2024-08-09 11:22:10 +0300

    Added MapTypeGuard to isUnion

--- src/isUnion.ts
+++ src/isUnion.ts
@@ -1,12 +1,12 @@
-import { TypeGuard, Guarded } from "./types";
+import { TypeGuard, MapTypeGuard } from "./types";
 import { isValue } from "./isValue";
 
-export const isUnion = <T extends TypeGuard<any>[]>(...guards: T): TypeGuard<Guarded<T[number]>> => {
-	return (value: unknown): value is Guarded<T[number]> => {
+export const isUnion = <T extends readonly unknown[]>(...guards: MapTypeGuard<T>): TypeGuard<T[number]> => {
+	return (value: unknown): value is T[number] => {
 		return guards.some(guard => guard(value));
 	};
 };
 
-export const isValueUnion = <const T extends unknown[]>(...values: T): TypeGuard<T[number]> => {
+export const isValueUnion = <const T extends readonly unknown[]>(...values: T): TypeGuard<T[number]> => {
 	return isUnion(...values.map(value => isValue(value)));
 };
