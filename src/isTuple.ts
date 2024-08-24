fced76177d2900a5e5be400df9585c2457d0e5cb
razhayat <razh.graffiti@gmail.com>
2024-08-24 16:39:27 +0300

    Extracting template now happens immediately

--- src/isTuple.ts
+++ src/isTuple.ts
@@ -2,12 +2,10 @@ import { TypeGuard, TypeGuardTemplate, TypeGuardTemplateParameter } from "./type
 import { extractTemplate } from "./utils";
 
 export const isTuple = <T extends readonly unknown[]>(template: TypeGuardTemplateParameter<T>): TypeGuard<T> => {
-	let resolvedTemplate: TypeGuardTemplate<T> | null = null;
-
 	const guard = (value: unknown): value is T => {
-		resolvedTemplate = resolvedTemplate ?? extractTemplate(template, guard);
 		return Array.isArray(value) && resolvedTemplate.length >= value.length && resolvedTemplate.every((guard, i) => guard(value[i]));
 	};
 
+	const resolvedTemplate = extractTemplate(template, guard);
 	return guard;
 };
