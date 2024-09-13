7723f97142558b37b5978c27ddb826b3beb6cee5
razhayat <razh.graffiti@gmail.com>
2024-09-13 14:52:40 +0300

    Added isRecord tests

---
+++ tests/isRecord.test.ts
@@ -0,0 +1,42 @@
+import { describe } from "vitest";
+import { describedGuardTests } from "./utils";
+import { isIndexRecord, isNumber, isRecord, isString, isValueUnion } from "../src";
+
+describe("is Record<string, number> record", () => {
+	describedGuardTests({
+		guard: isIndexRecord(isString, isNumber),
+		testCases: [
+			[null, false],
+			[undefined, false],
+			[21, false],
+			["4", false],
+			[new Date(), false],
+			[{ hello: "bye" }, false],
+			[{ hi: 12, bye: 6, blue: "kvdkdm" }, false],
+			[[], false],
+			[{}, true],
+			[{ hi: 12 }, true],
+			[{ hi: 12, bye: 6 }, true],
+			[{ [Symbol()]: 45 }, true],
+		],
+	});
+});
+
+describe("is Record<'a' | 'b', 'c', 'd'> record", () => {
+	describedGuardTests({
+		guard: isRecord(["a", "b"], isValueUnion("c", "d")),
+		testCases: [
+			[null, false],
+			[undefined, false],
+			[[], false],
+			[{}, false],
+			[new Set(), false],
+			[{ a: "c" }, false],
+			[{ a: "d", b: "invalid value" }, false],
+			[{ a: "invalid", b: "another invalid" }, false],
+			[{ a: "c", b: "d", another: "kvdkdm" }, true],
+			[{ a: "c", b: "d" }, true],
+			[{ a: "d", b: "c" }, true],
+		],
+	});
+});
