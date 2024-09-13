46d3accd593d0993735ab4523eb93c06268f1b52
razhayat <razh.graffiti@gmail.com>
2024-09-13 14:51:15 +0300

    Made distinction between isRecord and isIndexRecord

--- src/isRecord.ts
+++ src/isRecord.ts
@@ -1,7 +1,19 @@
 import { isObject } from "./isUtils";
 import { TypeGuard } from "./types";
 
-export const isRecord = <K extends PropertyKey, V>(isKey: TypeGuard<K>, isValue: TypeGuard<V>): TypeGuard<Record<K, V>> => {
+export const isRecord = <K extends PropertyKey[], V>(keys: K, isValue: TypeGuard<V>): TypeGuard<Record<K[number], V>> => {
+	return (value: unknown): value is Record<K[number], V> => {
+		if (!isObject(value) || value.constructor !== Object) {
+			return false;
+		}
+
+		return keys.every(key => {
+			return isValue(Reflect.get(value, key));
+		});
+	};
+};
+
+export const isIndexRecord = <K extends PropertyKey, V>(isKey: TypeGuard<K>, isValue: TypeGuard<V>): TypeGuard<Record<K, V>> => {
 	return (value: unknown): value is Record<K, V> => {
 		if (!isObject(value) || value.constructor !== Object) {
 			return false;
