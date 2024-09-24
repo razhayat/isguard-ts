03de451660c9ce3f53470d5e1a13e9488df6534d
razhayat <razh.graffiti@gmail.com>
2024-09-24 20:16:09 +0300

    Implemented isSet

---
+++ src/isSet.ts
@@ -0,0 +1,7 @@
+import { TypeGuard } from "./types";
+
+export const isSet = <T>(isValue: TypeGuard<T>): TypeGuard<Set<T>> => {
+	return (value: unknown): value is Set<T> => {
+		return value instanceof Set && [...value.values()].every(isValue);
+	};
+};
