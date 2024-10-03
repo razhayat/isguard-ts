eecbc1107fe7e89d63a18418e3c81e05cb951389
razhayat <razh.graffiti@gmail.com>
2024-10-03 22:02:54 +0300

    Implemented record util

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,8 @@ import { TypeGuard, TypeGuardTemplate, TypeGuardTemplateParameter } from "./type
 export const extractTemplate = <T>(parameter: TypeGuardTemplateParameter<T>, guard: TypeGuard<T>): TypeGuardTemplate<T> => {
 	return isFunction(parameter) ? extractTemplate(parameter(guard), guard) : parameter;
 };
+
+export const record = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>): TypeGuardTemplate<Record<K[number], V>> => {
+	const entries = keys.map((key: K[number]) => [key, isValue] as const);
+	return Object.fromEntries(entries) as TypeGuardTemplate<Record<K[number], V>>;
+};
