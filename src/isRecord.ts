12575ef2ee138bb967aa7c765a759fa81ec4650f
razhayat <razh.graffiti@gmail.com>
2024-10-05 15:38:27 +0300

    Added isPartialRecord

--- src/isRecord.ts
+++ src/isRecord.ts
@@ -1,10 +1,14 @@
 import { isType } from "./isType";
 import { isObject } from "./isUtils";
 import { TypeGuard } from "./types";
-import { record } from "./utils";
+import { partialRecord, record } from "./utils";
 
 export const isRecord = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>) => {
 	return isType(record(keys, isValue));
+};
+
+export const isPartialRecord = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>) => {
+	return isType(partialRecord(keys, isValue));
 };
 
 export const isIndexRecord = <K extends PropertyKey, V>(isValue: TypeGuard<V>): TypeGuard<Record<K, V>> => {
