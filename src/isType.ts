9b69eb9a5d127cf68f3df4c952578bbeb1e2d38b
razhayat <razh.graffiti@gmail.com>
2024-10-05 15:33:39 +0300

    Improved isType to accept tuples as well

--- src/isType.ts
+++ src/isType.ts
@@ -2,8 +2,14 @@ import { TypeGuardTemplateParameter, TypeGuard } from "./types";
 import { isNil } from "./isUtils";
 import { extractTemplate } from "./utils";
 
-export const isType = <T extends object>(template: T extends readonly unknown[] ? never : TypeGuardTemplateParameter<T>): TypeGuard<T> => {
-	const guard = (value: any): value is T => {
+type Pretty<T> = {
+	[K in keyof T]: T[K];
+} & {};
+
+export type IsTypeGuarded<T> = [T] extends [readonly unknown[]] ? Pretty<Omit<T, keyof unknown[]>> : T;
+
+export const isType = <T extends object>(template: TypeGuardTemplateParameter<T, IsTypeGuarded<T>>): TypeGuard<IsTypeGuarded<T>> => {
+	const guard = (value: any): value is IsTypeGuarded<T> => {
 		if (isNil(value)) {
 			return false;
 		}
@@ -17,6 +23,6 @@ export const isType = <T extends object>(template: T extends readonly unknown[] 
 		return true;
 	};
 
-	const resolvedTemplate = extractTemplate<T>(template, guard);
+	const resolvedTemplate = extractTemplate<T, IsTypeGuarded<T>>(template, guard);
 	return guard;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,10 @@
 import { isFunction } from "./isUtils";
 import { TypeGuard, TypeGuardTemplate, TypeGuardTemplateParameter } from "./types";
 
-export const extractTemplate = <T>(parameter: TypeGuardTemplateParameter<T>, guard: TypeGuard<T>): TypeGuardTemplate<T> => {
-	return isFunction(parameter) ? extractTemplate(parameter(guard), guard) : parameter;
+export const extractTemplate = <T, G = T>(parameter: TypeGuardTemplateParameter<T, G>, guard: TypeGuard<G>): TypeGuardTemplate<T> => {
+	return isFunction(parameter) ? extractTemplate<T, G>(parameter(guard), guard) : parameter;
 };
 
-/**
- * @internal
- */
 export const record = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>): TypeGuardTemplate<Record<K[number], V>> => {
 	const entries = keys.map((key: K[number]) => [key, isValue] as const);
 	return Object.fromEntries(entries) as TypeGuardTemplate<Record<K[number], V>>;
