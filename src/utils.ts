2e339c16fec613162d57601c5c41cbabb0c4aaf4
razhayat <razh.graffiti@gmail.com>
2024-10-05 14:29:10 +0300

    Made record internal

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,9 @@ export const extractTemplate = <T>(parameter: TypeGuardTemplateParameter<T>, gua
 	return isFunction(parameter) ? extractTemplate(parameter(guard), guard) : parameter;
 };
 
+/**
+ * @internal
+ */
 export const record = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>): TypeGuardTemplate<Record<K[number], V>> => {
 	const entries = keys.map((key: K[number]) => [key, isValue] as const);
 	return Object.fromEntries(entries) as TypeGuardTemplate<Record<K[number], V>>;
