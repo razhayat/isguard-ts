70bd9ae1a5840c252f7a19bc98e5e3c480363fc7
razhayat <razh.graffiti@gmail.com>
2024-10-05 15:37:06 +0300

    Introduced partial and partialRecord

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { isFunction } from "./isUtils";
+import { isFunction, isOptional } from "./isUtils";
 import { TypeGuard, TypeGuardTemplate, TypeGuardTemplateParameter } from "./types";
 
 export const extractTemplate = <T, G = T>(parameter: TypeGuardTemplateParameter<T, G>, guard: TypeGuard<G>): TypeGuardTemplate<T> => {
@@ -8,4 +8,17 @@ export const extractTemplate = <T, G = T>(parameter: TypeGuardTemplateParameter<
 export const record = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>): TypeGuardTemplate<Record<K[number], V>> => {
 	const entries = keys.map((key: K[number]) => [key, isValue] as const);
 	return Object.fromEntries(entries) as TypeGuardTemplate<Record<K[number], V>>;
+};
+
+const partial = <T>(template: TypeGuardTemplate<T>): TypeGuardTemplate<Partial<T>> => {
+	const partialTemplate = Array.isArray(template) ? [] : {};
+	for (const key in template) {
+		Object.assign(partialTemplate, { [key]: isOptional(template[key]) });
+	}
+
+	return partialTemplate as TypeGuardTemplate<Partial<T>>;
+};
+
+export const partialRecord = <const K extends readonly PropertyKey[], V>(keys: K, isValue: TypeGuard<V>) => {
+	return partial(record(keys, isValue));
 };
