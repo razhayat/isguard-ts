fbd518880d93b06e33057536aeafae43cb6a1086
razhayat <razh.graffiti@gmail.com>
2024-12-20 16:05:28 +0200

    Added is empty union tests

--- tests/isUnion.test.ts
+++ tests/isUnion.test.ts
@@ -2,6 +2,28 @@ import { describe } from "vitest";
 import { describedGuardTests } from "./utils";
 import { isBoolean, isDate, isNumber, isString, isUnion, isValueUnion } from "../src";
 
+describe("is empty union (never)", () => {
+	describedGuardTests({
+		guard: isUnion(),
+		testCases: [
+			[null, false],
+			[undefined, false],
+			[34363, false],
+			[412424n, false],
+			[-253.5332, false],
+			[true, false],
+			[false, false],
+			[() => {}, false],
+			[function() {}, false],
+			[new Date(), false],
+			[Array, false],
+			["hello", false],
+			[[], false],
+			[{}, false],
+		],
+	});
+});
+
 describe("is Date | number | string | boolean", () => {
 	describedGuardTests({
 		guard: isUnion(isDate, isNumber, isString, isBoolean),
