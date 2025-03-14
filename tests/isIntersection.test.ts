686ae0acc39417709b981edfd659c63aad1a11ca
razhayat <razh.graffiti@gmail.com>
2025-03-14 21:09:33 +0200

    Added nested isIntersection tests with the help of chat-gpt

--- tests/isIntersection.test.ts
+++ tests/isIntersection.test.ts
@@ -72,3 +72,72 @@ describe("is { a: number } & { b: string }", () => {
 		],
 	});
 });
+
+describe("is { obj: { a: string } } & { obj: { b: number } }", () => {
+	type A = {
+		obj: {
+			a: string;
+		};
+	};
+
+	const isA = isType<A>({
+		obj: isType<A["obj"]>({
+			a: isString,
+		}),
+	});
+
+	type B = {
+		obj: {
+			b: number;
+		};
+	};
+
+	const isB = isType<B>({
+		obj: isType<B["obj"]>({
+			b: isNumber,
+		}),
+	});
+
+	describedGuardTests({
+		guard: isIntersection(isA, isB),
+		testCases: [
+			[null, false],
+			[undefined, false],
+			[[], false],
+			[{}, false],
+			["{}", false],
+			[true, false],
+			[12, false],
+			[12n, false],
+			[new Date(), false],
+			[() => {}, false],
+			[Symbol(123), false],
+
+			[{ obj: { a: "foo" } }, false],
+			[{ obj: { a: "bar" } }, false],
+
+			[{ obj: { b: 3.14 } }, false],
+			[{ obj: { b: -100 } }, false],
+
+			[{ obj: null }, false],
+			[{ obj: undefined }, false],
+			[{ obj: { a: "hello", b: undefined } }, false],
+			[{ obj: { a: null, b: 12 } }, false],
+			[{ obj: { a: [], b: 100 } }, false],
+			[{ obj: { a: "hello", b: new Map() } }, false],
+
+			[{ objj: { a: "string", b: 0 } }, false],
+			[{ ob: { a: "string", b: 0 } }, false],
+			[{ obj: { a: "string", bb: 0 } }, false],
+			[{ obj: { aa: "string", b: 0 } }, false],
+
+			[{ obj: { a: "foo", b: 3.14 } }, true],
+			[{ obj: { a: "bar", b: -100 } }, true],
+			[{ obj: { a: "example", b: 0 } }, true],
+
+			[{ obj: { a: "world", b: 99 }, extra: "value" }, true],
+			[{ obj: { a: "world", b: 99 }, extra: null }, true],
+			[{ obj: { a: "world", b: 99 }, extra: new Date() }, true],
+		],
+	});
+});
