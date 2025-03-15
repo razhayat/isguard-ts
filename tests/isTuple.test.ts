47fb9a1f511ec2a9c7feb867c2923f2462d83081
razhayat <razh.graffiti@gmail.com>
2025-03-15 20:31:11 +0200

    Added more isTuple thanks to the gpt

--- tests/isTuple.test.ts
+++ tests/isTuple.test.ts
@@ -1,6 +1,6 @@
 import { describe } from "vitest";
 import { describedGuardTests } from "./utils";
-import { isTuple, isNumber, isOptional, isOptionalNumber, isString } from "../src";
+import { isTuple, isNumber, isOptional, isOptionalNumber, isString, isType } from "../src";
 
 describe("is normal tuple", () => {
 	type Tuple = [string, number];
@@ -83,6 +83,66 @@ describe("is recursive tuple", () => {
 			[[12, [13, [14]]], true],
 			[[11, [12, [13, [14]]]], true],
 			[[11, [12, [13, [14, undefined]]]], true],
+		],
+	});
+});
+
+describe("is tuple of objects", () => {
+	type Tuple = [{ a: string; }, { b: number; }];
+
+	describedGuardTests({
+		guard: isTuple<Tuple>([
+			isType({ a: isString }),
+			isType({ b: isNumber }),
+		]),
+		testCases: [
+			[null, false],
+			[undefined, false],
+			[NaN, false],
+			[123, false],
+			[1.23, false],
+			[123n, false],
+			["string", false],
+			[true, false],
+			[{}, false],
+			[[], false],
+			[Symbol(), false],
+			[() => {}, false],
+			[async () => {}, false],
+			[Promise.resolve(), false],
+
+			[["hello", 42], false],
+			[[42, "world"], false],
+			[["good", "bad"], false],
+			[[true, 100], false],
+
+			[[null, { b: 42 }], false],
+			[[undefined, { b: 42 }], false],
+			[[123, { b: 42 }], false],
+			[[true, { b: 42 }], false],
+
+			[[{ a: "❤️" }, Symbol()], false],
+			[[{ a: "ok" }, BigInt(123)], false],
+			[[{ a: "bye" }, () => {}], false],
+			[[{ a: "nice" }, Promise.resolve()], false],
+
+			[[{ b: "valid" }, { a: 4754 }], false],
+			[[{ b: 123 }, { a: "string" }], false],
+			[[{ c: "not acceptable" }, { a: "nicely done" }, { b: 438 }], false],
+			[[{ c: "not acceptable" }, { b: 754 }, { a: "welcome" }], false],
+
+			[[{ a: NaN }, { b: null }], false],
+			[[{ a: "valid" }, { notB: 123 }], false],
+			[[{ notA: "valid" }, { b: 123 }], false],
+			[[{ notB: "valid" }, { notA: 123 }], false],
+
+			[[{ a: "string" }, { b: NaN }, null], false],
+			[[{ a: "" }, { b: 1000 }, undefined], false],
+			[[{ a: "" }, { b: Infinity }, { c: new Date() }], false],
+
+			[[{ a: "test" }, { b: -1000 }], true],
+			[[{ a: "empty" }, { b: 0 }], true],
+			[[{ a: "valid" }, { b: Number.MAX_SAFE_INTEGER }], true],
 		],
 	});
 });
