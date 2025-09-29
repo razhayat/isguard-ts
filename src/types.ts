5e49729ef1e94fd458457c005078c437cf30c84a
razhayat <razh.graffiti@gmail.com>
2025-09-29 21:10:15 +0300

    Implemented .indexRecord

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,7 @@ export type TypeGuard<in out T, in out _U extends ExactEqual<T> = ExactEqual<T>>
 	maybe: () => TypeGuard<T | null>;
 	array: () => TypeGuard<T[]>;
 	set: () => TypeGuard<Set<T>>;
+	indexRecord: () => TypeGuard<Record<PropertyKey, T>>;
 	refine: <R extends T>(refinement: (value: T) => value is R) => TypeGuard<R>;
 };
 
