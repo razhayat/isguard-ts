562e592ea7a99792c27bfb84dfc7b9426f5d65a6
razhayat <razh.graffiti@gmail.com>
2025-10-12 15:07:38 +0300

    Made unbox a method

--- src/isOptional/index.ts
+++ src/isOptional/index.ts
@@ -3,7 +3,7 @@ import { TypeGuard } from "../types";
 import { OptionalTypeGuardClass } from "./internal";
 
 export type OptionalTypeGuard<T> = UnionTypeGuard<[undefined, T]> & {
-	unbox: () => TypeGuard<T>;
+	unbox(): TypeGuard<T>;
 };
 
 export const isOptional = <T>(guard: TypeGuard<T>): OptionalTypeGuard<T> => {
